be888a374ca61c63bd6d0fa712da68d5859c0aa0
SOPHIA <sophialeite@gmail.com>
2024-04-04 22:16:34 -0300

    Aciona Página de Receita

--- ReceitaApp/views.py
+++ ReceitaApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from ReceitaApp.models import Receita, Categoria
 
-# Create your views here.
+# Create your views here
+def index(request):
+    categorias = Categoria.objects.all()
+
+    context = {
+         'categorias' : categorias
+    }
+
+    return render(request, 'index.html', context)
+
+
+def listar_receitas(request):
+    #buscar as receitas no banco de dados utilizando ORM do Django
+
+    receitas = Receita.objects.all()
+    
+    #dicionario que vai levar os dados para o template
+    #chave : valor
+    context = {
+        'receitas' : receitas
+        
+    }
+
+    #qual template essa view vai retornar
+    return render(request, 'receitas.html', context)
+
+def detalhes_receita(request, id):
+    #buscando a receita correspondente ao id informado
+    receita = Receita.objects.get(id = id)
+
+    context = {
+        'receita' : receita
+
+    }
+    return render(request, 'receita.html', context)
